4e4eaea0a495a77f9a26e1a95ffa81714ebfd42f
ifeoluwa <ifeoluwaoluwadara@gmail.com>
2025-01-28 13:41:22 +0100

    registration_screen

--- lib/Screens/registration_screen.dart
+++ lib/Screens/registration_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -13,14 +14,70 @@ firstNameController=TextEditingController ();
 final TextEditingController
 LastNamecontroller=TextEditingController ();
 final TextEditingController
-emailName=TextEditingController ();
+emailController=TextEditingController ();
 bool isRegistered = false;
-}
+
 @override 
-Widget build(BuildContext) {
+Widget build(BuildContext ) {
 return Scaffold(
   appBar: AppBar(
     title: const Text( "Registration form")
   ),
-);
+  body: Padding(padding:const EdgeInsets.all(20),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      TextField(
+      controller: 
+        firstNameController,
+        decoration: const
+        InputDecoration(
+          labelText: "First Name"
+        ),
+      ),
+       TextField(
+          controller: 
+        LastNamecontroller,
+        decoration: const
+        InputDecoration(
+          labelText: "Last Name"
+        ),
+      ),
+       TextField(
+       controller: 
+       emailController,
+       decoration: const
+       InputDecoration(
+        labelText: "Email Name"
+       ),
+      ),
+     const SizedBox(
+      height: 30),
+     GestureDetector(
+      onTap: (){
+        setState(() {
+          isRegistered =!isRegistered;
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color:isRegistered? Colors.green : Colors.blue,
+           borderRadius: BorderRadius.circular(10)
+        ),
+        child: Text(isRegistered ?
+        "registration successful" :
+        "Register me",
+        style: const TextStyle(
+          color: Colors.white,
+        ),),
+      ),
+     )
+    ],
+  ),
+  )
+  
+  );
+
+}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Registration Form",
-      home: Registration Screen(),
+      home: RegistrationScreen(),
     );
     
     
